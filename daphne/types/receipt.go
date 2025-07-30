f1474c04e90b46b4448e918eb56faa753b545ea2
Filip Drobnjaković <drobnjakovicfilip@gmail.com>
2025-07-30 16:38:33 +0200

    Add Receipt type

---
+++ daphne/types/receipt.go
@@ -0,0 +1,14 @@
+package types
+
+import (
+	"fmt"
+)
+
+// Receipt represents the outcome of a Daphne transaction execution.
+type Receipt struct {
+	Success bool
+}
+
+func (r *Receipt) String() string {
+	return fmt.Sprintf("Success:%t", r.Success)
+}
