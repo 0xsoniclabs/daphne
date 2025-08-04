acffc1935d38363e5984e6c6c1296f8341a18084
Matija Kupresanin <87671707+kmeeth@users.noreply.github.com>
2025-08-04 21:55:48 +0200

    Document the Bundle type.

--- daphne/types/bundle.go
+++ daphne/types/bundle.go
@@ -2,6 +2,8 @@ package types
 
 import "fmt"
 
+// Bundle represents a verifiable product of consensus.
+// It contains a bundle number and a slice of transactions.
 type Bundle struct {
 	Number       uint32
 	Transactions []Transaction
