b481330dff40501b2c5d0503e55399d333bdc03a
Matija Kupresanin <matijakup@gmail.com>
2025-08-08 14:46:48 +0200

    Implement receiptStore for rpc

---
+++ daphne/rpc/store_test.go
@@ -0,0 +1,46 @@
+package rpc
+
+import (
+	"testing"
+
+	"github.com/0xsoniclabs/daphne/daphne/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReceiptStore_AddBlock_AddsValidBlock(t *testing.T) {
+	store := &receiptStore{}
+	tx1 := types.Transaction{Nonce: 1}
+	tx2 := types.Transaction{Nonce: 2}
+	block := types.Block{
+		Transactions: []types.Transaction{tx1, tx2},
+		Receipts:     []types.Receipt{{}, {}},
+	}
+
+	err := store.AddBlock(block)
+	require.NoError(t, err)
+
+	receipt, found := store.GetReceipt(tx1.Hash())
+	require.True(t, found)
+	require.NotNil(t, receipt)
+
+	receipt, found = store.GetReceipt(tx2.Hash())
+	require.True(t, found)
+	require.NotNil(t, receipt)
+}
+
+func TestReceiptStore_AddBlock_MismatchedTransactionsReceipts(t *testing.T) {
+	store := &receiptStore{}
+	tx1 := types.Transaction{
+		From:  1,
+		To:    2,
+		Value: 100,
+		Nonce: 1,
+	}
+	block := types.Block{
+		Transactions: []types.Transaction{tx1},
+		Receipts:     []types.Receipt{{}, {}}, // Mismatched count
+	}
+
+	err := store.AddBlock(block)
+	require.ErrorContains(t, err, "mismatched")
+}
