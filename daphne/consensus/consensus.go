1a670dc07583e5011b642390531251698df71b58
aksonic <amir@fantom.foundation>
2025-08-12 00:27:23 +1000

    Add consensus interfaces and documentation

---
+++ daphne/consensus/consensus.go
@@ -0,0 +1,46 @@
+package consensus
+
+//go:generate mockgen -source consensus.go -destination=consensus_mock.go -package=consensus
+
+import (
+	"github.com/0xsoniclabs/daphne/daphne/p2p"
+	"github.com/0xsoniclabs/daphne/daphne/types"
+)
+
+// Consensus defines the common functionality expected from all consensus
+// protocol implementations.
+type Consensus interface {
+	// RegisterListener registers a listener which is notified on bundle creation.
+	// Bundles are the common output of any consensus protocol. This is
+	// implemented as a callback because each implementation may commit bundles
+	// based on different event inputs and/or timing. The fact that consensus
+	// will eventually be reached is the only common interface.
+	RegisterListener(listener BundleListener)
+}
+
+// Factory defines the methods required to instantiate a consensus protocol
+// implementation.
+type Factory interface {
+	// NewActive creates a new active consensus instance that emits bundles
+	// at a configured interval, using the provided source transaction provider
+	// to get candidate transactions.
+	NewActive(p2p.Server, TransactionProvider) Consensus
+	// NewPassive creates a new passive consensus instance that does not emit
+	// bundles but listens for them from its peers. It can verify and linearize
+	// bundles but it does not contribute to bundle creation.
+	NewPassive(p2p.Server) Consensus
+}
+
+// TransactionProvider is a component that returns candidate transactions
+// for linearization in the consensus protocol. These transactions are consumed
+// by teh consensus protocol implementation from a pool of user-submitted
+// transactions to form bundles which are then linearized.
+type TransactionProvider interface {
+	GetCandidateTransactions() []types.Transaction
+}
+
+// BundleListener is an interface that defines the common behaviour expected
+// from listeners such as block processors or other components.
+type BundleListener interface {
+	OnNewBundle(bundle types.Bundle)
+}
