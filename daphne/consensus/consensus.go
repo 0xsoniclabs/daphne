1ceac5fa961d9fd822a1b011d94bff9cb5df1519
aksonic <164125348+aksonic@users.noreply.github.com>
2025-08-12 20:22:59 +1000

    Add consensus interfaces and message type for bundles (#29)
    * Add consensus interfaces and documentation
    
    * Update consensus documentation
    
    * Update consensus documentation
    
    * Update consensus documentation

---
+++ daphne/consensus/consensus.go
@@ -0,0 +1,47 @@
+package consensus
+
+//go:generate mockgen -source consensus.go -destination=consensus_mock.go -package=consensus
+
+import (
+	"github.com/0xsoniclabs/daphne/daphne/p2p"
+	"github.com/0xsoniclabs/daphne/daphne/types"
+)
+
+// Consensus defines the common functionality expected from all consensus
+// protocol implementations.
+type Consensus interface {
+	// RegisterListener registers a listener which is notified on bundle creation.
+	// Bundles are the common output of any consensus protocol. This is
+	// implemented as a callback because each implementation may commit bundles
+	// based on different event inputs and/or timing. The fact that consensus
+	// will eventually be reached is the only common interface.
+	RegisterListener(listener BundleListener)
+}
+
+// Factory defines the methods required to instantiate a consensus protocol
+// implementation.
+type Factory interface {
+	// Every consensus instance, whether active or passive, emits bundles.
+	// NewActive creates a new active consensus instance. Only active consensus
+	// instances contribute to the decision-making, or consensus, to reach a
+	// quorum to both create and emit new bundles.
+	NewActive(p2p.Server, TransactionProvider) Consensus
+	// NewPassive creates a new passive consensus instance. A passive consensus
+	// instance is a listener without decision-making power in consensus,
+	// that is, it only observes, and potentially broadcasts, messages from the
+	// rest of the network to reach the same conclusions/recalculate which
+	// bundles to emit that active members would propose/create.
+	NewPassive(p2p.Server) Consensus
+}
+
+// TransactionProvider is a component that returns candidate transactions
+// for linearization in the consensus protocol.
+type TransactionProvider interface {
+	GetCandidateTransactions() []types.Transaction
+}
+
+// BundleListener is an interface that defines the common behaviour expected
+// from listeners such as block processors or other components.
+type BundleListener interface {
+	OnNewBundle(bundle types.Bundle)
+}
