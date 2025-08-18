417e16535b2cc16aa5cc1dc995a32e5b217b2684
Matija Kupresanin <matijakup@gmail.com>
2025-08-18 12:46:16 +0200

    Add test for error resistance

--- daphne/generic/gossip_test.go
+++ daphne/generic/gossip_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/0xsoniclabs/daphne/daphne/p2p"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
 
@@ -67,4 +68,29 @@ func Test_Gossip_Broadcast_BroadcastingMessageKnownToPeerDoesNotSendMessage(t *t
 	// Broadcast the message again, which is now known to the peer
 	// (this will not send the message).
 	gossip.Broadcast(uint32(1))
+}
+
+func Test_Gossip_Broadcast_BroadcastErrorDoesNotMarkMessageAsKnown(t *testing.T) {
+	p2pServer := p2p.NewMockServer(gomock.NewController(t))
+
+	// Server has one peer.
+	p2pServer.EXPECT().GetPeers().Return([]p2p.PeerId{"peer1"}).AnyTimes()
+	// Expect the SendMessage to return an error.
+	p2pServer.EXPECT().SendMessage(p2p.PeerId("peer1"), p2p.Message{
+		Code:    p2p.MessageCode_TxGossip_NewTransaction,
+		Payload: uint32(1),
+	}).Return(fmt.Errorf("send error")).Times(1)
+	// This method is irrelevant for the test.
+	p2pServer.EXPECT().RegisterMessageHandler(gomock.Any()).AnyTimes()
+
+	gossip := NewGossip(p2pServer, func(msg uint32) string {
+		return fmt.Sprintf("%d", msg)
+	})
+
+	// Broadcast a message that will fail to send.
+	gossip.Broadcast(uint32(1))
+
+	// Check if the message is known to the peer.
+	known := gossip.isTransactionKnownByPeer(p2p.PeerId("peer1"), uint32(1))
+	require.False(t, known, "Message should not be marked as known after a failed send")
 }
