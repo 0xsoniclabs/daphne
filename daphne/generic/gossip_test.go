8cabeecdfa0fd96a06d4e6ab512b9faad61958d3
Matija Kupresanin <matijakup@gmail.com>
2025-08-18 12:46:16 +0200

    Add some tests for generic broadcast

---
+++ daphne/generic/gossip_test.go
@@ -0,0 +1,46 @@
+package generic
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/0xsoniclabs/daphne/daphne/p2p"
+	"go.uber.org/mock/gomock"
+)
+
+func Test_NewGossip(t *testing.T) {
+	// Create a mock P2P server.
+	p2pServer := p2p.NewMockServer(gomock.NewController(t))
+	// Expect the RegisterMessageHandler to be called once, for the gossip instance.
+	p2pServer.EXPECT().RegisterMessageHandler(gomock.Any()).Times(1)
+
+	// Create a new gossip instance
+	_ = NewGossip(p2pServer, func(msg uint32) string {
+		return fmt.Sprintf("%d", msg)
+	})
+}
+
+func Test_Gossip_Broadcast_AllPeersReceiveMessage(t *testing.T) {
+	p2pServer := p2p.NewMockServer(gomock.NewController(t))
+
+	// Server has two peers.
+	p2pServer.EXPECT().GetPeers().Return([]p2p.PeerId{"peer1", "peer2"}).AnyTimes()
+	// Expect the SendMessage to be called for each peer.
+	p2pServer.EXPECT().SendMessage(p2p.PeerId("peer1"), p2p.Message{
+		Code:    p2p.MessageCode_TxGossip_NewTransaction,
+		Payload: uint32(1),
+	}).Return(nil).Times(1)
+	p2pServer.EXPECT().SendMessage(p2p.PeerId("peer2"), p2p.Message{
+		Code:    p2p.MessageCode_TxGossip_NewTransaction,
+		Payload: uint32(1),
+	}).Return(nil).Times(1)
+	// This method is irrelevant for the test.
+	p2pServer.EXPECT().RegisterMessageHandler(gomock.Any()).AnyTimes()
+
+	gossip := NewGossip(p2pServer, func(msg uint32) string {
+		return fmt.Sprintf("%d", msg)
+	})
+
+	// Broadcast a message.
+	gossip.Broadcast(uint32(1))
+}
