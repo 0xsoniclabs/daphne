e0c2b3919aaf29c91b7fc8bc33b41c3aea0f6827
Matija Kupresanin <matijakup@gmail.com>
2025-08-20 11:35:16 +0200

    Add newline

--- daphne/generic/broadcast.go
+++ daphne/generic/broadcast.go
@@ -1,6 +1,7 @@
 package generic
 
-// /go:generate mockgen -source broadcast.go -destination=broadcast_mock.go -package=generic
+// go:generate mockgen -source broadcast.go -destination=broadcast_mock.go -package=generic
+
 // Broadcaster is a policy for broadcasting messages of type M to multiple receivers.
 // No assumptions are made about the distribution policy - they could be best-effort
 // only or a guaranteed delivery protocol, with or without timing guarantees.
