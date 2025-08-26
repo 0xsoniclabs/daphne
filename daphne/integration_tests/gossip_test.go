362af8677ff29572779dbf387bb32e54a2636ef8
Luis Ayuso <luis@ph3.at>
2025-08-26 14:22:05 +0200

    refactor integration test
    to avoid race conditions.

--- daphne/integration_tests/gossip_test.go
+++ daphne/integration_tests/gossip_test.go
@@ -2,25 +2,14 @@ package integrationtests
 
 import (
 	"fmt"
-	"maps"
-	"slices"
 	"testing"
+	"time"
 
 	"github.com/0xsoniclabs/daphne/daphne/generic"
 	"github.com/0xsoniclabs/daphne/daphne/p2p"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
-
-// testReceiver is a simple implementation of BroadcastReceiver for testing purposes.
-type testReceiver struct {
-	f func(message p2p.PeerId)
-}
-
-func (r *testReceiver) OnMessage(message p2p.PeerId) {
-	if r.f != nil {
-		r.f(message)
-	}
-}
 
 func TestGossip_BroadcastWorksWithP2pServer(t *testing.T) {
 	// This test checks if the gossip broadcast works correctly with the P2P server.
@@ -28,38 +17,49 @@ func TestGossip_BroadcastWorksWithP2pServer(t *testing.T) {
 	network := p2p.NewNetwork()
 	servers := make([]p2p.Server, 5)
 	for i := range 5 {
-		server, err := network.NewServer(p2p.PeerId(fmt.Sprintf("%d", i+1)))
+		server, err := network.NewServer(toPeerId(i + 1))
 		require.NoError(t, err, "Failed to create server %d", i+1)
 		servers[i] = server
 	}
 
-	// Keeps track of which peer has received messages from which peers.
-	peerReceivedRecord := make(map[p2p.PeerId]map[p2p.PeerId]struct{})
+	ctrl := gomock.NewController(t)
 
 	gossips := make([]generic.Broadcaster[p2p.PeerId], 5)
 	for i, server := range servers {
 		gossip := generic.NewGossip(server, func(msg p2p.PeerId) p2p.PeerId {
 			return msg
 		}, p2p.MessageCode_TxGossip_NewTransaction)
-		gossip.RegisterReceiver(&testReceiver{
-			f: func(message p2p.PeerId) {
-				myId := p2p.PeerId(fmt.Sprintf("%d", i+1))
-				if _, exists := peerReceivedRecord[myId]; !exists {
-					peerReceivedRecord[myId] = make(map[p2p.PeerId]struct{})
-				}
-				peerReceivedRecord[myId][p2p.PeerId(message)] = struct{}{}
-			},
-		})
+
+		receiver := generic.NewMockBroadcastReceiver[p2p.PeerId](ctrl)
+
+		minimum := func(j int) int {
+			// a node may hear its own messages back from the network, but it
+			// is not mandatory
+			if i+1 == j {
+				return 0
+			}
+			// messages originated from another id
+			// must be received at least once
+			return 1
+		}
+
+		receiver.EXPECT().OnMessage(toPeerId(1)).MinTimes(minimum(1))
+		receiver.EXPECT().OnMessage(toPeerId(2)).MinTimes(minimum(2))
+		receiver.EXPECT().OnMessage(toPeerId(3)).MinTimes(minimum(3))
+		receiver.EXPECT().OnMessage(toPeerId(4)).MinTimes(minimum(4))
+		receiver.EXPECT().OnMessage(toPeerId(5)).MinTimes(minimum(5))
+
+		gossip.RegisterReceiver(receiver)
 		gossips[i] = gossip
 	}
 
 	for i := range 5 {
-		gossips[i].Broadcast(p2p.PeerId(fmt.Sprintf("%d", i+1)))
+		gossips[i].Broadcast(toPeerId(i + 1))
 	}
 
-	for i := range 5 {
-		// Each peer should have received messages from all other peers.
-		require.ElementsMatch(t, []p2p.PeerId{"1", "2", "3", "4", "5"},
-			slices.Collect(maps.Keys(peerReceivedRecord[p2p.PeerId(fmt.Sprintf("%d", i+1))])))
-	}
+	time.Sleep(10 * time.Millisecond)
+}
+
+func toPeerId(i int) p2p.PeerId {
+	return p2p.PeerId(fmt.Sprintf("server%d", i))
 }
