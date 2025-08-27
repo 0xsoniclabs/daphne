21ff8bcb0a43785c5276b69c3da3905e8bac48e8
Matija Kupresanin <matijakup@gmail.com>
2025-08-27 15:37:55 +0200

    Improve tests

--- daphne/txpool/txpool_test.go
+++ daphne/txpool/txpool_test.go
@@ -294,7 +294,7 @@ func TestTxPool_AddAndContains_AreThreadSafe(t *testing.T) {
 	wg.Wait()
 }
 
-func TestTxPool_AddingToOnePoolWithoutErrorCausesOtherPoolToReceiveTransaction(t *testing.T) {
+func TestTxGossip_AddingToOnePoolWithoutErrorCausesOtherPoolToReceiveTransaction(t *testing.T) {
 	network := p2p.NewNetwork()
 
 	server1, err := network.NewServer(p2p.PeerId("peer1"))
@@ -315,7 +315,7 @@ func TestTxPool_AddingToOnePoolWithoutErrorCausesOtherPoolToReceiveTransaction(t
 	require.True(t, pool2.Contains(tx.Hash()))
 }
 
-func TestTxPool_AddingToOnePoolWithErrorDoesNotBroadcastTransaction(t *testing.T) {
+func TestTxGossip_AddingToOnePoolWithErrorDoesNotBroadcastTransaction(t *testing.T) {
 	network := p2p.NewNetwork()
 
 	server1, err := network.NewServer(p2p.PeerId("peer1"))
@@ -351,43 +351,21 @@ func TestInstallTxGossip_RegistersHandlers(t *testing.T) {
 	InstallTxGossip(pool, server)
 }
 
-func TestTxGossip_HandleMessage_AddsTransactionToPoolSuccessfully(t *testing.T) {
+func TestTxGossip_MessageReceiverAdapter_AddsTransactionToPoolSuccessfully(t *testing.T) {
 	ctrl := gomock.NewController(t)
-
-	server := p2p.NewMockServer(ctrl)
 	pool := NewMockTxPool(ctrl)
-	// These methods are irrelevant for the test.
-	server.EXPECT().RegisterMessageHandler(gomock.Any()).AnyTimes()
-	pool.EXPECT().RegisterListener(gomock.Any()).AnyTimes()
-
-	// No peers are expected to be returned, so we can test the message handling.
-
-	// without it being broadcast further.
-	server.EXPECT().GetPeers().Return([]p2p.PeerId{})
 	pool.EXPECT().Add(types.Transaction{}).Return(nil)
-	txGossip := installTxGossip(pool, server)
-	txGossip.HandleMessage(p2p.PeerId("peer1"), p2p.Message{
-		Code:    p2p.MessageCode_TxGossip_NewTransaction,
-		Payload: types.Transaction{},
-	})
+
+	adapter := messageReceiverAdapter{pool}
+	adapter.OnMessage(types.Transaction{})
 }
 
 func TestTxGossip_HandleMessage_AddsTransactionToPoolWithError(t *testing.T) {
 	ctrl := gomock.NewController(t)
-
-	server := p2p.NewMockServer(ctrl)
 	pool := NewMockTxPool(ctrl)
-	// These methods are irrelevant for the test.
-	server.EXPECT().RegisterMessageHandler(gomock.Any()).AnyTimes()
-	pool.EXPECT().RegisterListener(gomock.Any()).AnyTimes()
-
-	// No peers are expected to be returned, so we can test the message handling.
-	server.EXPECT().GetPeers().Return([]p2p.PeerId{})
 	pool.EXPECT().Add(types.Transaction{}).Return(errors.New("test error"))
-	txGossip := installTxGossip(pool, server)
+
+	adapter := messageReceiverAdapter{pool}
 	// Expect that the error is logged, but nothing else happens.
-	txGossip.HandleMessage(p2p.PeerId("peer1"), p2p.Message{
-		Code:    p2p.MessageCode_TxGossip_NewTransaction,
-		Payload: types.Transaction{},
-	})
+	adapter.OnMessage(types.Transaction{})
 }
