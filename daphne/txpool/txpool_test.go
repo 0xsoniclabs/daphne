c2db7e6119e482696edb7f7f83d686a28ff9a11d
Matija Kupresanin <matijakup@gmail.com>
2025-08-27 15:37:55 +0200

    Clarify test

--- daphne/txpool/txpool_test.go
+++ daphne/txpool/txpool_test.go
@@ -361,7 +361,7 @@ func TestTxGossip_HandleMessage_AddsTransactionToPoolWithError(t *testing.T) {
 	server.EXPECT().GetPeers().Return([]p2p.PeerId{})
 	pool.EXPECT().Add(gomock.Any()).Return(errors.New("test error"))
 	txGossip := installTxGossip(pool, server)
-	// Expect that the error is logged, but the transaction is still handled.
+	// Expect that the error is logged, but the transaction is still added to the pool.
 	txGossip.HandleMessage(p2p.PeerId("peer1"), p2p.Message{
 		Code:    p2p.MessageCode_TxGossip_NewTransaction,
 		Payload: types.Transaction{},
