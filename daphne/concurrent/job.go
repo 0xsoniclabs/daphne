e39f6291d3f4838799a7421b65fcc726263e32c5
Herbert <herbert.jordan.jun@gmail.com>
2025-09-02 10:00:54 +0200

    Add utility for handling async jobs (#61)

---
+++ daphne/concurrent/job.go
@@ -0,0 +1,86 @@
+package concurrent
+
+import (
+	"math/rand/v2"
+	"time"
+)
+
+// Job is a handle to an operation running in another goroutine that can be
+// stopped on demand. It is intended to simplify the creation and management of
+// background operations that need to be stopped at some point.
+//
+// A typical use case would look like this:
+//
+//	job := StartPeriodicJob(time.Second, func(t time.Time) {
+//	    // Do some work...
+//	})
+//
+// where it can be eventually stopped using
+//
+//	job.Stop()
+//
+// Operations on a Job are not thread-safe.
+type Job struct {
+	stop chan<- struct{}
+	done <-chan struct{}
+}
+
+// Stop signals the job to stop and waits for it to finish. A second call to
+// Stop or attempting to stop a non-running job has no effect.
+func (j *Job) Stop() {
+	if j.stop == nil {
+		return
+	}
+	close(j.stop)
+	<-j.done
+	j.stop = nil
+	j.done = nil
+}
+
+// StartJob creates a new goroutine running the given task in the background
+// until it either completes or Stop is called on the returned Job. The task
+// should use the provided channel to detect stop requests.
+func StartJob(task func(<-chan struct{})) *Job {
+	stop := make(chan struct{})
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		task(stop)
+	}()
+	return &Job{
+		stop: stop,
+		done: done,
+	}
+}
+
+// StartPeriodicJob creates a new goroutine running the given task at regular
+// intervals until Stop is called on the returned Job. The first execution will
+// happen after a random interval between 0 and the given period to avoid
+// clustering of periodic jobs with the same period in the system.
+func StartPeriodicJob(
+	period time.Duration,
+	task func(time.Time),
+) *Job {
+	return StartJob(func(stop <-chan struct{}) {
+		// Do a random offset at the beginning to avoid clustering of
+		// periodic jobs with the same period.
+		firstDelay := time.Duration(float64(period) * rand.Float64())
+		select {
+		case time := <-time.After(firstDelay):
+			task(time)
+		case <-stop:
+			return
+		}
+		// After the first run, switch to a ticker.
+		ticker := time.NewTicker(period)
+		defer ticker.Stop()
+		for {
+			select {
+			case time := <-ticker.C:
+				task(time)
+			case <-stop:
+				return
+			}
+		}
+	})
+}
---
+++ daphne/concurrent/job_test.go
@@ -0,0 +1,86 @@
+package concurrent
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJob_Stop_CanBeCalledOnZeroJob(t *testing.T) {
+	job := Job{}
+	job.Stop()
+}
+
+func TestJob_Stop_CanBeCalledMoreThanOnce(t *testing.T) {
+	job := StartPeriodicJob(time.Second, func(time.Time) {
+		t.Errorf("unexpected call to task")
+	})
+	job.Stop()
+	job.Stop() // < no panic for closing a closed channel
+}
+
+func TestJob_Stop_IsSignaledToJobForGracefulShutdown(t *testing.T) {
+	job := StartJob(func(stop <-chan struct{}) {
+		<-stop // would block forever if not signaled
+	})
+	job.Stop() // < if it completes, the job has finished
+}
+
+func TestJob_Stop_BlocksUntilJobIsFinished(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		start := time.Now()
+		job := StartJob(func(stop <-chan struct{}) {
+			time.Sleep(time.Second)
+		})
+		job.Stop()
+		require.Equal(t, time.Since(start), time.Second)
+	})
+}
+
+func TestJob_CanStartAndStopPeriodicJob(t *testing.T) {
+	period := 1 * time.Second
+	synctest.Test(t, func(t *testing.T) {
+		counter := 0
+		done := make(chan struct{})
+		start := time.Now()
+		last := start
+		job := StartPeriodicJob(period, func(time time.Time) {
+			delay := time.Sub(last)
+			if last.Equal(start) {
+				require.LessOrEqual(
+					t, delay, period,
+					"initial delay was too high",
+				)
+			} else {
+				require.Equal(
+					t, delay, period,
+					"unexpected interval between periodic calls",
+				)
+			}
+			last = time
+			counter++
+			if counter == 3 {
+				close(done)
+			}
+		})
+
+		select {
+		case <-time.After(5 * time.Second):
+			t.Error("failed to run jobs within expected time")
+		case <-done:
+		}
+		job.Stop()
+	})
+}
+
+func TestJob_CanStopPeriodicJobBeforeFirstTrigger(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		period := 1 * time.Second
+		job := StartPeriodicJob(period, func(time.Time) {
+			t.Errorf("unexpected call to task")
+		})
+		job.Stop()
+	})
+}
