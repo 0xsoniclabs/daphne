36e3c60f6c7f70101ead8009ede0fa5f7d1e91cf
Matija Kupresanin <matijakup@gmail.com>
2025-10-14 14:05:41 +0200

    Rename creator to validator in all contexts where creator does not make sense

--- daphne/consensus/validator_test.go
+++ daphne/consensus/validator_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreator_Serialize(t *testing.T) {
-	creatorId := ValidatorId(10000)
-	serialized := creatorId.Serialize()
+func TestValidator_Serialize(t *testing.T) {
+	validatorId := ValidatorId(10000)
+	serialized := validatorId.Serialize()
 	expected := []byte{0x00, 0x00, 0x27, 0x10} // 10000 in big-endian format
 
 	require.Equal(t, expected, serialized, "Serialized data should match expected byte slice")
