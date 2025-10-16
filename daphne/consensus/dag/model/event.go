a15481b3e641da32d3a237793580a6f72b56f886
Filip Drobnjaković <drobnjakovicfilip@gmail.com>
2025-10-16 16:56:17 +0200

    Event Traverse Closure

--- daphne/consensus/dag/model/event.go
+++ daphne/consensus/dag/model/event.go
@@ -10,6 +10,8 @@ import (
 	"github.com/0xsoniclabs/daphne/daphne/utils/sets"
 )
 
+//go:generate mockgen -source event.go -destination=event_mock.go -package=model
+
 type EventId types.Hash
 
 func (c EventId) Serialize() []byte {
