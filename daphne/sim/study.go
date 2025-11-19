526996d330e94381bf8b6cb7982864ed15f26b7d
Matija Kupresanin <matijakup@gmail.com>
2025-11-19 16:22:21 +0100

    Remove duplicate

--- daphne/sim/study.go
+++ daphne/sim/study.go
@@ -276,24 +276,6 @@ func getConsensusProtocolStudy() Study {
 					EmitInterval:    500 * time.Millisecond,
 					LayeringFactory: lachesis.Factory{},
 				},
-				tendermint.Factory{
-					ProposePhaseTimeout:   100 * time.Millisecond,
-					PrevotePhaseTimeout:   100 * time.Millisecond,
-					PrecommitPhaseTimeout: 100 * time.Millisecond,
-					PhaseTimeoutDelta:     10 * time.Millisecond,
-				},
-				tendermint.Factory{
-					ProposePhaseTimeout:   250 * time.Millisecond,
-					PrevotePhaseTimeout:   250 * time.Millisecond,
-					PrecommitPhaseTimeout: 250 * time.Millisecond,
-					PhaseTimeoutDelta:     10 * time.Millisecond,
-				},
-				tendermint.Factory{
-					ProposePhaseTimeout:   500 * time.Millisecond,
-					PrevotePhaseTimeout:   500 * time.Millisecond,
-					PrecommitPhaseTimeout: 500 * time.Millisecond,
-					PhaseTimeoutDelta:     10 * time.Millisecond,
-				},
 			)),
 			Dim(Topology{}, List[p2p.NetworkTopology](
 				p2p.NewFullyMeshedTopology(),
